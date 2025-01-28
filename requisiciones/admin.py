5c43f33a2653328c5af4638acf19df452bb9451c
UlisesHuesca <ulises_huesc@hotmail.com>
2025-01-28 14:42:17 -0600

    Se agrega campo de busqueda por folio en el admnistrador de la tabla requis

--- requisiciones/admin.py
+++ requisiciones/admin.py
@@ -4,6 +4,7 @@ from .models import Salidas, Requis, ArticulosRequisitados, ValeSalidas, Devoluc
 class RequisAdmin(admin.ModelAdmin):
     list_display = ('id','folio','orden','autorizar')
     list_filter = ('folio',)
+    search_fields = ['folio']
     raw_id_fields = ('orden',)
 
 class ValeSalidasAdmin(admin.ModelAdmin):
