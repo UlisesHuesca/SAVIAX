4908d2d5916f8ebd4ae25b09214a3d66c9f4ad34
ulises.huesca <ulises.huesc@hotmail.com>
2025-11-10 18:46:01 -0600

    Arreglando el render

--- requisiciones/filters.py
+++ requisiciones/filters.py
@@ -31,17 +31,12 @@ class Historical_Articulos_surtir_Filter(django_filters.FilterSet):
     solicitud = CharFilter(field_name='articulos__orden__folio', lookup_expr='icontains')
     producto = CharFilter(field_name='articulos__producto__producto__nombre', lookup_expr='icontains')
     codigo = CharFilter(field_name='articulos__producto__producto__codigo', lookup_expr='icontains')
-    #nombre = CharFilter(field_name='articulos__orden__staff__staff__first_name', lookup_expr='icontains')
-   #nombre = CharFilter(method ='my_custom_filter', label="Search")
-    #apellido =CharFilter(field_name='articulos__orden__staff__staff__last_name', lookup_expr='icontains')
-    proyecto = CharFilter(field_name='articulos__orden__proyecto__nombre', lookup_expr='icontains')
-    subproyecto = CharFilter(field_name='articulos__orden__subproyecto__nombre', lookup_expr='icontains')
-    start_date = DateFilter(field_name = 'articulos__orden__approved_at', lookup_expr='gte')
-    end_date = DateFilter(field_name='articulos__orden__approved_at',lookup_expr='lte')
+    start_date = DateFilter(field_name='history_date', lookup_expr='gte')
+    end_date = DateFilter(field_name='history_date', lookup_expr='lte')
 
     class Meta:
-        model = ArticulosparaSurtir
-        fields = ['solicitud','producto','codigo','start_date','end_date',]
+        model = ArticulosparaSurtir.history.model
+        fields = ['solicitud','producto','codigo','start_date','end_date']
 
 
 class SalidasFilter(django_filters.FilterSet):
