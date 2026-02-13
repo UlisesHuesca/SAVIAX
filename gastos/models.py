93ae21210c800dd19af5ed3dee063cae203c2848
ulises.huesca <ulises.huesc@hotmail.com>
2026-02-13 17:22:00 -0600

    Se quitan el iva de los gastos, estos estaban en los modelos de los gastos mediante properties

--- gastos/models.py
+++ gastos/models.py
@@ -67,12 +67,12 @@ class Solicitud_Gasto(models.Model):
         total = sum([producto.get_subtotal for producto in productos])
         return total
 
-    @property
-    def get_total_impuesto(self):
-        productos = self.articulos.all()
-        productos = productos.filter(completo=True)
-        suma = round(sum([(producto.get_iva + producto.get_otros_impuestos) for producto in productos]),2)
-        return suma
+    #@property
+    #def get_total_impuesto(self):
+    #    productos = self.articulos.all()
+    #    productos = productos.filter(completo=True)
+    #    suma = round(sum([(producto.get_iva + producto.get_otros_impuestos) for producto in productos]),2)
+    #    return suma
 
     @property
     def get_total_solicitud(self):
@@ -171,8 +171,9 @@ class Articulo_Gasto(models.Model):
 
     @property
     def total_parcial(self):
-        impuesto = self.get_iva
-        total = round(self.get_subtotal + impuesto + self.get_otros_impuestos, 2)
+        #impuesto = self.get_iva
+        # total = round(self.get_subtotal + impuesto +self.get_otros_impuestos, 2)
+        total = round(self.get_subtotal + self.get_otros_impuestos, 2)
         return total
 
 class Factura(models.Model):
