6cb26e818b3db23529a343ce6b9812da9da1c780
ulises.huesca <ulises.huesc@hotmail.com>
2026-04-23 09:53:50 -0600

    Se actualiza el admin de articulos comprados para poder hacer búsquedas por folio

--- compras/admin.py
+++ compras/admin.py
@@ -10,7 +10,7 @@ class CompraAdmin(admin.ModelAdmin):
 class ArticuloComprado_Admin(admin.ModelAdmin):
     list_display = ('oc','producto','cantidad')
     raw_id_fields = ['oc','producto'] 
-    search_fields = ['producto__producto__articulos__producto__producto__nombre']
+    search_fields = ['producto__producto__articulos__producto__producto__nombre', 'producto__oc__id']
     raw_id_fields = ('producto','oc',)
 
 class ProveedorAdmin(admin.ModelAdmin):
