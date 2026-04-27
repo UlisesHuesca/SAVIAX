d5456253956667713fc467baab8ba58d53c25862
ulises.huesca <ulises.huesc@hotmail.com>
2026-04-27 17:49:53 -0600

    Se descubre que no lo hacia para las liberaciones de calidad v2

--- compras/views.py
+++ compras/views.py
@@ -2416,15 +2416,15 @@ def convert_excel_matriz_compras(compras):
         else:
             tipo_producto = "PRODUCTO/SERVICIOS"
         
-        hay_no_liberados = compra.vale_entrada.filter(
-            articulos__liberado=False
+        hay_pendientes = compra.vale_entrada.filter(
+            Q(articulos__liberado=False) | Q(articulos__calidad=False)
         ).exists()
 
         if compra.entrada_completa:
             entrada_text = 'Entregado'
         elif compra.recepcion_completa and compra.solo_servicios:
             entrada_text = 'Entregado'
-        elif hay_no_liberados:
+        elif hay_pendientes:
             entrada_text = 'Faltan liberaciones de calidad'
         else: 
             entrada_text ='No Entregado'
